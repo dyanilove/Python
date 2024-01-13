ace5d20b79408e88e263b1e96fe98bec5e85b9a5
dyanilove <ekdms216@nate.com>
2024-01-13 16:35:41 +0900

    study

---
+++ variable1.py
@@ -0,0 +1,29 @@
+# 변수의 선언
+var = 42
+
+# print(var) # 42
+
+# print(var + 10)
+# print(var -5)
+# print(var /10)
+# print(var // 10)
+# print(var % 10)
+
+var2 = var / 10
+
+# print(var>var2)
+
+var_float = 3.14
+# print(3.14 * 6)
+# print(3.14 / 2)
+
+is_true = True
+is_false = False
+
+print(is_true and is_true) # True
+print(is_true and is_false) # False
+print(is_false and is_false) # False
+
+print(is_true or is_true) # True
+print(is_true or is_false) # True
+print(is_false or is_false) # False
